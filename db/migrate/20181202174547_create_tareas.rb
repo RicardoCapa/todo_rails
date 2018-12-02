dfc919f460acb3b3677c57be89e33cb714252329
RicardoCapa <angel.capa@unl.edu.ec>
2018-12-02 13:11:18 -0500

    agregado modelo y controlador Tareas

--- db/migrate/20181202174547_create_tareas.rb
+++ db/migrate/20181202174547_create_tareas.rb
@@ -1,8 +1,9 @@
 class CreateTareas < ActiveRecord::Migration
   def change
     create_table :tareas do |t|
-
-      t.timestamps null: false
+    	t.string :descripcion, null:false
+    	t.boolean :hecho, default:false
+      	t.timestamps null: false
     end
   end
 end
